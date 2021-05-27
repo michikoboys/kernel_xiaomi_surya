b5dae7f7d2400e938be1095ddfd27a230c97e9da
John Galt <johngaltfirstrun@gmail.com>
2021-05-27 11:09:18 -0400

    msm: kgsl: Make mem workqueue freezable
    Freezing during no interactivity may benefit power
    
    Change-Id: I685806f9d39da1f3523ba70590797de926840e18
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -5316,7 +5316,6 @@ static int __init kgsl_core_init(void)
 	}
 
 	kgsl_driver.mem_workqueue = alloc_workqueue("kgsl-mementry",
-		WQ_UNBOUND | WQ_MEM_RECLAIM, 0);
 
 	if (!kgsl_driver.mem_workqueue) {
 		pr_err("kgsl: Failed to allocate mem workqueue\n");
